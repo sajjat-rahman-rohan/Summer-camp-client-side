9b577506f8b225000b47700aa4ea4caab3d530d3
mdrohan <mdsajed210@gmail.com>
2023-06-11 17:44:25 +0600

    added use instructors

---
+++ src/hooks/useInstructors.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+
+const useInstructors = () => {
+  const { user, loading } = useAuth();
+  // const token = localStorage.getItem('access-token');
+  const [axiosSecure] = useAxiosSecure();
+  const { refetch, data: instructor = [] } = useQuery({
+    queryKey: ["instructors"],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure("/instructors");
+      console.log("res from axios", res);
+      return res.data;
+    },
+  });
+
+  return [instructor, refetch];
+};
+export default useInstructors;
