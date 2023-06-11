17c40b131576e96b5da19b9e3d59530bb43c2ee0
mdrohan <mdsajed210@gmail.com>
2023-06-11 17:42:12 +0600

    added dashboard instructor classes total enrollded student

---
+++ src/pages/Dashboard/Instructor/TotalEnrolledStudents.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const TotalEnrolledStudents = () => {
+  return (
+    <div>
+      <h2>Total student enrolled coming</h2>
+    </div>
+  );
+};
+
+export default TotalEnrolledStudents;
