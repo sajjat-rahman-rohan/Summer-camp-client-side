a600c64e2663303b4275373e3efcba19e1b7fb89
mdrohan <mdsajed210@gmail.com>
2023-06-11 17:36:21 +0600

    added use classes hook

---
+++ src/hooks/useClasses.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+
+const useClasses = () => {
+  const { user, loading } = useAuth();
+  // const token = localStorage.getItem('access-token');
+  const [axiosSecure] = useAxiosSecure();
+  const { refetch, data: classes = [] } = useQuery({
+    queryKey: ["classes"],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure("/classes");
+      console.log("res from axios", res);
+      return res.data;
+    },
+  });
+
+  return [classes, refetch];
+};
+export default useClasses;
