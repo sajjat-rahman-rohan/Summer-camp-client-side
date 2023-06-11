9cfce81ddd8755b09b68bc60396e16479cc0831e
mdrohan <mdsajed210@gmail.com>
2023-06-11 17:24:06 +0600

    added instructor route

---
+++ src/Routes/InstructorRoute.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../hooks/useAuth";
+import useAdmin from "../hooks/useAdmin";
+import useInstructor from "../hooks/useInstructor";
+
+const InstructorRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const [isInstructor, isInstructorLoading] = useInstructor();
+  const location = useLocation();
+
+  if (loading || isInstructorLoading) {
+    return <progress className="progress w-56"></progress>;
+  }
+
+  if (user && isInstructor) {
+    return children;
+  }
+  return <Navigate to="/" state={{ from: location }} replace></Navigate>;
+};
+
+export default InstructorRoute;
